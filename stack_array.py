7a4e66209bdfd557efedd439245729d923bc8bca
Nilay Patel <npatel84@calpoly.edu>
2021-01-28 11:59:14 -0700

    Hello

--- stack_array.py
+++ stack_array.py
@@ -10,28 +10,47 @@ class Stack:
     def is_empty(self):
         '''Returns True if the stack is empty, and False otherwise
            MUST have O(1) performance'''
+        if self.num_items == 0:
+            return True
+        return False
 
     def is_full(self):
         '''Returns True if the stack is full, and False otherwise
            MUST have O(1) performance'''
-
+        if self.num_items == self.capacity:
+            return True
+        return False
+        
     def push(self, item):
         '''If stack is not full, pushes item on stack. 
            If stack is full when push is attempted, raises IndexError
            MUST have O(1) performance'''
+        if self.is_full():
+            raise IndexError
+        self.items[self.num_items] = item
+        self.num_items += 1
 
     def pop(self): 
         '''If stack is not empty, pops item from stack and returns item.
            If stack is empty when pop is attempted, raises IndexError
            MUST have O(1) performance'''
+        if self.is_empty():
+            raise IndexError
+        self.num_items -= 1
+        return self.items[self.num_items]
 
     def peek(self):
         '''If stack is not empty, returns next item to be popped (but does not pop the item)
            If stack is empty, raises IndexError
            MUST have O(1) performance'''
+        if self.is_empty():
+            raise IndexError
+        return self.items[self.num_items-1]
 
     def size(self):
         '''Returns the number of elements currently in the stack, not the capacity
            MUST have O(1) performance'''
+        return self.num_items
+        
 
  
